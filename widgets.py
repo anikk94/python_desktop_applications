aea071066bf61816b4e8618873a92b6c0ff3d819
Anirudh Krishnan <anirudhkrishnanisavailable@gmail.com>
2022-06-09 14:00:30 -0400

    age calculator checkpoint 1

--- widgets.py
+++ widgets.py
@@ -9,6 +9,11 @@ w1.grid(column=0, row=0)
 
 w2 = tk.Button(window, text="click me")
 w2.grid(column=0, row=1)
+w3 = tk.Checkbutton(window, text="checkbutton", variable=tk.IntVar())
+w3.grid(column=0, row=2)
+
+w4 = tk.Label(window, text="label widget")
+w4.grid(column=0, row=3)
 
 
 window.mainloop()
